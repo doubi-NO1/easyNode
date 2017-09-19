973e4fa2c9858bd88e15a7c6ceb40ea0c7f8c499
xiaona.wang <xiaona.wang@ezhiyang.com>
2017-09-19 14:00:42 +0800

    async 不支持

--- test/dbMongoTest.js
+++ test/dbMongoTest.js
@@ -1,76 +1,48 @@
-const Mongo = require('../core/db/dbMongo.js');
+let should = require('should');
+let dbMongo = require('../core/db/dbMongo.js');
+let runtime = require('babel-plugin-transform-runtime');
+let polyfill = require('babel-polyfill');
 
+// function getMongo1(){
+//   return  dbMongo({
+//     dbAddress: "mongodb://47.94.207.219:27017/test"
+//   });
+// };
+// function getMongo2(){
+//   return dbMongo({
+//     dbAddress: "mongodb://47.94.207.219:27017/test"
+//   });
+// };
+// function getMongo3(){
+//   return dbMongo({
+//     dbAddress: "mongodb://47.94.207.219:27017/test"
+//   });
+// };
 
-const config = {
-  dbAddress: ''
-};
-
-(async() => {
-  let mongo;
-  try {
-    mongo = await Mongo(config);
-  } catch (e) {
-    console.log(e);
-  }
-
-  try {
-    console.log('新增成功', await mongo.insert({
-      data: {
-        name: '测试数据n',
-        description: '啊哈？'
-      },
-      tbName: 'tb_test'
-    }));
-  } catch (e) {
-    console.log('新增失败', e);
-  }
-
-  try {
-    console.log('查询多条成功', await mongo.find({
-      tbName: 'tb_test',
-      terms: {
-        name: '测试数据n'
-      }
-    }));
-  } catch (e) {
-    console.log('查询多条失败', e);
-  }
-
-  try {
-    console.log('查询一条成功', await mongo.findOne({
-      tbName: 'tb_test',
-      terms: {
-        name: '测试数据n'
-      }
-    }));
-  } catch (e) {
-    console.log('查询一条失败', e);
-  }
-
-  try {
-    console.log('更新成功', await mongo.update({
-      tbName: 'tb_test',
-      terms: {
-        name: '测试数据n'
-      },
-      data: {
-        description: '啊哈哈',
-        name: '测试数据n',
-        url: 'https://www.baidu.com'
-      }
-    }));
-  } catch (e) {
-    console.log('更新失败', e);
-  }
-
-  try {
-    console.log('删除成功', await mongo.remove({
-      tbName: 'tb_test',
-      terms: {
-        name: '测试数据n'
-      }
-    }));
-  } catch (e) {
-    console.log('删除失败', e);
-  }
+(async () => {
+  console.log('aaa');
+  // try {
+  //   // let [mongo1,mongo2,mongo3] = await Promise.all([getMongo1(), getMongo2(), getMongo3()]);
+  //   // let mongo1 = getMongo1();
+  //   describe("Mongo构造函数", async () =>{
+  //     it("Mongo的实例应该是唯一的", () => {
+  //       // mongo1.should.be.eql(mongo2);
+  //       "1".should.be.eql("1");
+  //     });
+  //   });
+  // } catch(e){
+  //   console.log(e);
+  // }
 })();
+  // it("实例化Mongo时，传入参数的属性dbAddress值如果为空应该返回'不能没有数据库地址'", function(){
+
+  // });
+  // it("实例化Mongo时，如果传入参数的属性dbAddress有值，应该可以连上此数据库", function(){
+
+  // });
+
+// describe("Mongo构造函数原型对象", function(){
+//   it("", function(){
+
+//   });
+// });
