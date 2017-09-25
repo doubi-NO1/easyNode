e13f9e0372914a63c937c63ae4d45f94dc3db87d
xiaona.wang <xiaona.wang@ezhiyang.com>
2017-09-25 18:05:47 +0800

    unit test dbmongo.js and dbMysql.js

--- test/dbMongoTest.js
+++ test/dbMongoTest.js
@@ -11,17 +11,24 @@ let searchTerms = "";
 
 (async () => {
   try {
-    describe('Mongo构造函数', () => {
-      it('当传入的参数为空时应该抛出类型错误提示信息', async () => {
+    describe('Mongo构造函数传参', () => {
+      it('当传入的参数为null时应该抛出类型错误提示信息', async () => {
         try {
           let mongo1 = await dbMongo(); 
         } catch(e) {
-          console.log(e);
-          let msg = e.es;
-          msg.should.containEql('null');
+          let msg = e.ok;
+          msg.should.eql(-1);
         }
       });
-      it('当传入的参数为空对象时应该抛出"不能没有数据库地址"', async () => {
+      it('当传入的参数为String时应该抛出"不能没有数据库地址"', async () => {
+        try {
+          let mongo1 = await dbMongo('172.123.12.1'); 
+        } catch(e) {
+          let msg = e.es;
+          msg.should.eql('不能没有数据库地址');
+        }
+      });
+      it('当传入的参数为{}时应该抛出"不能没有数据库地址"', async () => {
         try {
           let mongo1 = await dbMongo({}); 
         } catch(e) {
@@ -30,81 +37,224 @@ let searchTerms = "";
         }
       });
     });
+    describe('数据库地址', () => {
+      it('当传入错误的数据库地址时应该抛出错误提示信息', async () => {
+        try {
+          let mongo1 = await dbMongo({
+            'dbAddress': '47.94.207.219:27017'
+          });
+        } catch(e) {
+          let msg = e.ok;
+          msg.should.eql(-1);
+        }
+      }); 
+    });
     describe('Mongo构造函数方法', () => {
-      // describe('#insert()', () => {
-      //   it("插入数据后返回的对象的n属性应该为插入的条数", async () => {
-      //     let mongo1 = await dbMongo({
-      //       'dbAddress': address
-      //     });
-      //     let data = await mongo1.insert({
-      //       tbName: tbName,
-      //       data: [{
-      //         "name": "liwei",
-      //         "age": "12",
-      //         "sex":"男"
-      //       },{
-      //         "name": "张三",
-      //         "age": "23",
-      //         "sex": "nv"
-      //       }]
-      //     });
-      //     data.n.should.eql(2);
-      //   });
-      // });
-        // it("#find() 查找所有信息", async () => {
-        //   let mongo1 = await dbMongo({
-        //     'dbAddress': address
-        //   });
-        //   let data = await mongo1.find({
-        //     'tbName': tbName
-        //   });
-        //   let list = data.list;
-        //   list.should.not.be.empty;
-        // });
-        // it('#findOne()查找符合条件的信息', async () => {
-        //   let mongo1 = await dbMongo({
-        //     'dbAddress': address
-        //   });
-        //   let data = await mongo1.findOne({
-        //     'tbName': tbName,
-        //     'terms': {
-        //       'name': '张三'
-        //     }
-        //   });
-        //   let name = data.obj.name;
-        //   name.should.be.eql('张三');
-        // });
-        // it('#update()将name值为liwei的全部替换成lili', async () => {
-        //   let mongo1 = await dbMongo({
-        //     'dbAddress': address
-        //   });
-        //   let data = await mongo1.update({
-        //     'tbName': tbName,
-        //     'terms': {
-        //       'name': 'liwei'
-        //     },
-        //     'data':{
-        //       'name': 'lili'
-        //     }
-        //   });
-        //   let ok = data.ok;
-        //   ok.should.be.eql(1);
-        // });
-        // it('#remove()删除数据', async () => {
-        //   let mongo1 = await dbMongo({
-        //     'dbAddress': address
-        //   });
-        //   let data = await mongo1.remove({
-        //     'tbName': tbName,
-        //     'terms': {
-        //       'name': 'lili',
-        //       'age': '12'
-        //     }
-        //   });
-        //   let ok = data.ok;
-        //   ok.should.be.eql(1);
-        // });
-       
+      describe('#insert()', () => {
+        it("插入数据后返回的对象的n属性应该为插入的条数", async () => {
+          let mongo1 = await dbMongo({
+            'dbAddress': address
+          });
+          let data = await mongo1.insert({
+            tbName: tbName,
+            data: [{
+              "name": "liwei",
+              "age": "12",
+              "sex":"男"
+            },{
+              "name": "张三",
+              "age": "23",
+              "sex": "nv"
+            }]
+          });
+          data.n.should.eql(2);
+        });
+        it("插入的为空数据会抛出错误", async () => {
+          try {
+            let mongo1 = await dbMongo({
+              'dbAddress': address
+            });
+            let data = await mongo1.insert({
+              tbName: tbName,
+              data: []
+            });
+          } catch (e) {
+            let ok = e.ok;
+            ok.should.eql(-1);
+          }
+        });
+        it("没有传data参数时会抛出错误", async () => {
+          try {
+            let mongo1 = await dbMongo({
+              'dbAddress': address
+            });
+            let data = await mongo1.insert({
+              tbName: tbName
+            });
+          } catch (e) {
+            let ok = e.ok;
+            ok.should.eql(-1);
+          }
+        });
+      });
+      describe('#find()', () => {
+        it("#find() 查找所有信息", async () => {
+          let mongo1 = await dbMongo({
+            'dbAddress': address
+          });
+          let data = await mongo1.find({
+            'tbName': tbName
+          });
+          let list = data.list;
+          list.should.not.be.empty;
+        });
+        it("调用find方法时传入错误的数据库地址时应该抛出错误信息", async () => {
+          try {
+            let mongo1 = await dbMongo({
+              'dbAddress': '47.94.207.219:27017'
+            });
+            let data = await mongo1.find({
+              'tbName': tbName
+            });
+          } catch(e) {
+            console.log(e);
+            let msg = e.ok;
+            msg.should.eql(-1);
+          }
+        });
+        it('#findOne()查找符合条件的信息', async () => {
+          let mongo1 = await dbMongo({
+            'dbAddress': address
+          });
+          let data = await mongo1.findOne({
+            'tbName': tbName,
+            'terms': {
+              'name': '张三'
+            }
+          });
+          let name = data.obj.name;
+          name.should.be.eql('张三');
+        });
+      });
+      describe('#update()', () => {
+        it('将name值为liwei的全部替换成lili', async () => {
+          let mongo1 = await dbMongo({
+            'dbAddress': address
+          });
+          let data = await mongo1.update({
+            'tbName': tbName,
+            'terms': {
+              'name': 'liwei'
+            },
+            'data':{
+              'name': 'lili'
+            }
+          });
+          let ok = data.ok;
+          ok.should.be.eql(1);
+        });
+        it('terms为空时会把数据库里所有的数据都替换', async () => {
+          let mongo1 = await dbMongo({
+            'dbAddress': address
+          });
+          let data = await mongo1.update({
+            'tbName': tbName,
+            'terms': {
+            },
+            'data':{
+              'name': 'lili'
+            }
+          });
+          let ok = data.ok;
+          ok.should.be.eql(1);
+        });
+        it('data为空时会抛出错误信息', async ()=> {
+          try {
+            let mongo1 = await dbMongo({
+              'dbAddress': address
+            });
+            let data = await mongo1.update({
+              'tbName': tbName,
+              'terms': {
+              },
+              'data':{
+              }
+            });
+           
+          } catch(e) {
+            let ok = e.ok;
+            ok.should.be.eql(-1);
+          }
+          
+        });
+        it('不传terms对象时会抛出错误信息', async ()=> {
+          try {
+            let mongo1 = await dbMongo({
+              'dbAddress': address
+            });
+            let data = await mongo1.update({
+              'tbName': tbName,
+              'data':{
+              }
+            });
+          } catch(e) {
+            let ok = e.ok;
+            ok.should.be.eql(-1);
+          }
+        });
+        it('不传data对象时会抛出错误信息', async ()=> {
+          try {
+            let mongo1 = await dbMongo({
+              'dbAddress': address
+            });
+            let data = await mongo1.update({
+              'tbName': tbName,
+              'terms': {}
+            });
+          } catch(e) {
+            let ok = e.ok;
+            ok.should.be.eql(-1);
+          }
+        });
+      });
+      describe('#remove()', () => {
+        it('删除相应条件的数据', async () => {
+          let mongo1 = await dbMongo({
+            'dbAddress': address
+          });
+          let data = await mongo1.remove({
+            'tbName': tbName,
+            'terms': {
+              'name': 'lili',
+              'age': '12'
+            }
+          });
+          let ok = data.ok;
+          ok.should.be.eql(1);
+        });
+        it('terms为空对象时将所有的数据都删除', async ()=> {
+            let mongo1 = await dbMongo({
+              'dbAddress': address
+            });
+            let data = await mongo1.remove({
+              'tbName': tbName,
+              'terms': {
+              }
+            });
+            let ok = data.ok;
+            ok.should.eql(1);
+        });
+        it('不传terms对象时会删除所有数据', async ()=> {
+            let mongo1 = await dbMongo({
+              'dbAddress': address
+            });
+            let data = await mongo1.remove({
+              'tbName': tbName
+            });
+            let ok = data.ok;
+            ok.should.eql(1);
+        });
+      });
     });
   } catch(e) {
     console.log(e);
--- test/dbMysqlTest.js
+++ test/dbMysqlTest.js
@@ -1,94 +1,274 @@
-let Mysql = require('../core/db/dbMysql.js');
-let mysql = new Mysql({
-  connectionLimit: 10,
-  host: '',
-  port: '3306',
-  user: 'root',
-  password: '7crH5H7KbuHx5YdL',
-  database: 'test',
-});
+// let Mysql = require('../core/db/dbMysql.js');
+// let mysql = new Mysql({
+//   connectionLimit: 10,
+//   host: '',
+//   port: '3306',
+//   user: 'root',
+//   password: '7crH5H7KbuHx5YdL',
+//   database: 'test',
+// });
+
+// (async() => {
+//   try {
+//     console.log('新增单条成功', await mysql.insert({
+//       tbName: 'tb_test',
+//       data: {
+//         name: '测试数据n',
+//         description: '啊哈哈b'
+//       }
+//     }));
+//   } catch (e) {
+//     console.log('新增单条失败', e);
+//   }
+// })();
+
+// (async() => {
+//   try {
+//     console.log('新增多条成功', await mysql.insert({
+//       tbName: 'tb_test',
+//       data: [{
+//         name: '测试数据4',
+//         description: '啊哈哈4'
+//       }, {
+//         name: '测试数据5',
+//         description: '啊哈哈5'
+//       }, {
+//         name: '测试数据6',
+//         description: '啊哈哈6'
+//       }]
+//     }));
+//   } catch (e) {
+//     console.log('新增多条失败', e);
+//   }
+// })();
+
+// (async() => {
+//   try {
+//     console.log('查询成功', await mysql.select({
+//       tbName: 'tb_test',
+//       terms: {
+//         name: '测试数据n',
+//         description: '啊哈哈'
+//       }
+//     }));
+//   } catch (e) {
+//     console.log('查询失败', e);
+//   }
+// })();
+
+// (async() => {
+//   try {
+//     console.log('更新成功', await mysql.update({
+//       tbName: 'tb_test',
+//       data: {
+//         description: 'description2'
+//       },
+//       terms: [{
+//         field: 'name',
+//         value: '%测试数据%',
+//         term: 'like'
+//       }, {
+//         field: 'description',
+//         value: '%啊哈哈%',
+//         term: 'like'
+//       }]
+//     }));
+//   } catch (e) {
+//     console.log('更新失败', e);
+//   }
+// })();
+
+// (async() => {
+//   try {
+//     console.log('删除成功', mysql.remove({
+//       tbName: 'tb_test1',
+//       terms: [{
+//         field: 'name',
+//         value: '%test%',
+//         term: 'like'
+//       }]
+//     }));
+//   } catch (e) {
+//     console.log('删除失败', e);
+//   }
+// })();
+let mySql = require('../core/db/dbMysql.js');
+let should = require('should');
+let mocha = require('mocha');
+let runtime = require('babel-plugin-transform-runtime');
+let polyfill = require('babel-polyfill');
+
+let tbName = 'tb_test';
 
 (async() => {
-  try {
-    console.log('新增单条成功', await mysql.insert({
-      tbName: 'tb_test',
-      data: {
-        name: '测试数据n',
-        description: '啊哈哈b'
-      }
-    }));
-  } catch (e) {
-    console.log('新增单条失败', e);
-  }
-})();
-
-(async() => {
-  try {
-    console.log('新增多条成功', await mysql.insert({
-      tbName: 'tb_test',
-      data: [{
-        name: '测试数据4',
-        description: '啊哈哈4'
-      }, {
-        name: '测试数据5',
-        description: '啊哈哈5'
-      }, {
-        name: '测试数据6',
-        description: '啊哈哈6'
-      }]
-    }));
-  } catch (e) {
-    console.log('新增多条失败', e);
-  }
-})();
-
-(async() => {
-  try {
-    console.log('查询成功', await mysql.select({
-      tbName: 'tb_test',
-      terms: {
-        name: '测试数据n',
-        description: '啊哈哈'
-      }
-    }));
-  } catch (e) {
-    console.log('查询失败', e);
-  }
-})();
-
-(async() => {
-  try {
-    console.log('更新成功', await mysql.update({
-      tbName: 'tb_test',
-      data: {
-        description: 'description2'
-      },
-      terms: [{
-        field: 'name',
-        value: '%测试数据%',
-        term: 'like'
-      }, {
-        field: 'description',
-        value: '%啊哈哈%',
-        term: 'like'
-      }]
-    }));
-  } catch (e) {
-    console.log('更新失败', e);
-  }
-})();
-
-(async() => {
-  try {
-    console.log('删除成功', mysql.remove({
-      tbName: 'tb_test1',
-      terms: [{
-        field: 'name',
-        value: '%test%',
-        term: 'like'
-      }]
-    }));
-  } catch (e) {
-    console.log('删除失败', e);
-  }
+    try {
+        describe('mySql', () => {
+            describe('#insert()', () => {
+                it('增加数据-数组格式', async() => {
+                    try {
+                        let sql = await new mySql({
+                            host: '47.94.207.219',
+                            port: '3306',
+                            user: 'root',
+                            password: '7crH5H7KbuHx5YdL',
+                            database: 'test',
+                         });
+                        let data = await sql.insert({
+                            tbName: tbName,
+                            data: [{
+                                id:'76',
+                                name: 'aaa',
+                                description: 'aaa'
+                            },{
+                                id:'77',
+                                name: 'ccc',
+                                description: 'ccc'
+                            },{
+                                id:'78',
+                                name: 'vvv',
+                                description: 'ccc'
+                            }]
+                        });
+                        let total = data.total;
+                        total.should.be.eql(3);
+                    } catch(e) {
+                        console.log(e);
+                    }
+                });
+            });
+            describe('#insert()', () => {
+                it('增加数据-对象格式', async() => {
+                    try {
+                        let sql = await new mySql({
+                            host: '47.94.207.219',
+                            port: '3306',
+                            user: 'root',
+                            password: '7crH5H7KbuHx5YdL',
+                            database: 'test',
+                         });
+                        let data = await sql.insert({
+                            tbName: tbName,
+                            data: {
+                                id:'76',
+                                name: 'aaa',
+                                description: 'aaa'
+                            }
+                        });
+                        let total = data.total;
+                        total.should.be.eql(3);
+                    } catch(e) {
+                        console.log(e);
+                    }
+                });
+            });
+            describe('#update()', () => {
+                it('更新数据', async () => {
+                    try {
+                        let sql = await new mySql({
+                            connectionLimit: 10,
+                            host: '47.94.207.219',
+                            port: '3306',
+                            user: 'root',
+                            password: '7crH5H7KbuHx5YdL',
+                            database: 'test',
+                        });
+                        let data = await sql.update({
+                            tbName: 'tb_test',
+                            data: {
+                                description: 'description2'
+                            },
+                            terms: [{
+                                field: 'name',
+                                value: '%6%',
+                                term: 'like'
+                            }, {
+                                field: 'description',
+                                value: '%啊哈哈%',
+                                term: 'like'
+                            }]
+                        });
+                        let es = data.es;
+                        es.should.be.eql('success');
+                    } catch (e) {
+                        console.log(e);
+                    }
+                });
+            });
+            describe('#select()', () => {
+                it('选择数据', async ()=>{
+                    try {
+                        let sql = await new mySql({
+                            connectionLimit: 10,
+                            host: '47.94.207.219',
+                            port: '3306',
+                            user: 'root',
+                            password: '7crH5H7KbuHx5YdL',
+                            database: 'test',
+                        });
+                        let data = await sql.select({
+                            tbName: tbName,
+                            terms: {
+                                name: '14',
+                                description: 'description%'
+                            }
+                        });
+                        let es = data.es;
+                        es.should.be.eql('success');
+                    } catch(e) {
+                        console.log(e);
+                    }
+                });
+            });
+           describe('#remove()', () => {
+                it('移除数据-数组形式', async () => {
+                    try {
+                        let sql = await new mySql({
+                            host: '47.94.207.219',
+                            port: '3306',
+                            user: 'root',
+                            password: '7crH5H7KbuHx5YdL',
+                            database: 'test',
+                         });
+                         let data = await sql.remove({
+                            tbName: 'tb_test1',
+                            terms: [{
+                                field: 'name',
+                                value: '%4',
+                                term: 'like'
+                            }]
+                         });
+                         let es = data.es;
+                         es.should.be.eql('success');
+                    } catch(e) {
+                        console.log(e);
+                    }
+                });
+                it('移除数据-对象形式', async () => {
+                    try {
+                        let sql = await new mySql({
+                            host: '47.94.207.219',
+                            port: '3306',
+                            user: 'root',
+                            password: '7crH5H7KbuHx5YdL',
+                            database: 'test',
+                         });
+                         let data = await sql.remove({
+                            tbName: 'tb_test1',
+                            terms: {
+                                field: 'name',
+                                value: '%4',
+                                term: 'like'
+                            }
+                         });
+                         let es = data.es;
+                         es.should.be.eql('success');
+                    } catch(e) {
+                        console.log(e);
+                    }
+                });
+           });
+        });
+    } catch(e) {
+        console.log(e);
+    }
 })();
